b69dbe067ce918a9411c0275a52fda154c95ddd4
jvyden <jvyden420@gmail.com>
2019-07-13 22:38:23 -0400

    fix login

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ console.log("Starting Bluepillbot")
 console.log("Loading Libraries...")
 const Discord = require('discord.js');
 const fs = require('fs')
+
+bluepillid = "591085044471431178"
+
 console.log("Connecting to Discord...")
 const client = new Discord.Client();
 client.on('message', async function(message) {
   username = message.author.username
-  if(username === "HifumiBOT") {
-    return
-  }
   if(message.channel instanceof Discord.DMChannel) {
     console.log(`${username} (DM): ${message.toString()}`)
   } else {
     console.log(`${username} (#${message.channel.name}, ${message.guild.name}): ${message.toString()}`)
   }
-  await message.react("591085044471431178")
+  await message.react(bluepillid)
 })
 
 client.on('ready', () => {
   console.log(`Connected to Discord.`);
 });
-const token = fs.readFileSync('token.txt').toString().replace('\n', '')
+const token = fs.readFileSync('token.txt').toString().slice(0, -2)
 client.login(token)
